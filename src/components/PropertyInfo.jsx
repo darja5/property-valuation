17672fa4c1b763640ce8f4b922c5b8a38830b8cb
Darja Peternel <darja@Darjas-MacBook-Air.local>
2025-10-23 23:34:50 +0200

    Add collapsible elements and icons

--- src/components/PropertyInfo.jsx
+++ src/components/PropertyInfo.jsx
@@ -1,12 +1,36 @@
 import '../components/PropertyInfo.css';
+import { useState } from 'react';
+import Building from '../icons/buildings.svg';
+import ChevronUp from '../icons/chevron-up.svg';
+import ChevronDown from '../icons/chevron-down.svg';
+import VectorSquare from '../icons/vector-square.svg';
 
 export default function PropertyInfo({ location }) {
+  const [isOpenLandInfo, setIsOpenLandInfo] = useState(false);
+  const [isOpenUnitsInfo, setIsOpenUnitsInfo] = useState(false);
+
+  const generalLabels = ['reg. num', 'district', 'municipality', 'quarter'];
+  const landLabels = ['reg. num', 'area', 'plZone', ''];
+  const unitLabels = ['reg. num', 'type', 'extents', 'plZone'];
+
+  /*ali lahko to napišem samo kot stringe in jih po njih iščem JA :)
+    const unitLabels = [
+        { key: "regNumber", label: "Reg. number" },
+        { key: "buildingType", label: "Type" },
+        { key: "extents", label: "Extents" },
+        { key: "planningZone", label: "plZone" },
+        { key: "value", label: "Value" }
+    ];*/
+
   return (
-    <>
-      <div className="property-info">
-        {location.map((att) => {
-          let label = att.label.toUpperCase();
-          let value = att.value.toUpperCase();
+    <div className="property-info">
+      <div className="general-info">
+        {generalLabels.map((item) => {
+          let value;
+          if (item === 'reg. num')
+            value = location.generalInfo['regNumber'].toUpperCase();
+          else value = location.generalInfo[item].toUpperCase();
+          let label = item.toUpperCase();
           return (
             <>
               <label className="info-label">{label}</label>
@@ -15,10 +39,70 @@ export default function PropertyInfo({ location }) {
           );
         })}
       </div>
-      Collapsible Land
-      <br />
-      Collapsible Unit
-      <br />
-    </>
+
+      <button
+        type="button"
+        class={`collapsible ${isOpenLandInfo && 'open'}`}
+        onClick={() => setIsOpenLandInfo((open) => !open)}
+      >
+        <img
+          className="icon"
+          src={VectorSquare}
+          alt="Vector square"
+          style={{ width: '18px' }}
+        />
+        Land ({location.land.length})
+        {isOpenLandInfo ? (
+          <img className="chevron" src={ChevronUp} alt="Chevron up" />
+        ) : (
+          <img className="chevron" src={ChevronDown} alt="Chevron down" />
+        )}
+      </button>
+
+      {isOpenLandInfo && (
+        <div class="content">
+          {landLabels.map((item) => (
+            <div>{item.toUpperCase()}</div>
+          ))}
+          {location.land.map((item) => (
+            <>
+              <div>{item.regNumber}</div>
+              <div>{item.area}</div>
+              <div>{item.plZone}</div>
+              <div className="empty" />
+            </>
+          ))}
+        </div>
+      )}
+
+      <button
+        type="button"
+        class={`collapsible ${isOpenUnitsInfo && 'open'}`}
+        onClick={() => setIsOpenUnitsInfo((open) => !open)}
+      >
+        <img className="icon" src={Building} alt="Building" />
+        Units ({location.units.length})
+        {isOpenUnitsInfo ? (
+          <img className="chevron" src={ChevronUp} alt="Chevron up" />
+        ) : (
+          <img className="chevron" src={ChevronDown} alt="Chevron down" />
+        )}
+      </button>
+      {isOpenUnitsInfo && (
+        <div class="content">
+          {unitLabels.map((item) => (
+            <div>{item.toUpperCase()}</div>
+          ))}
+          {location.units.map((item) => (
+            <>
+              <div>{item.regNumber}</div>
+              <div>{item.type}</div>
+              <div>{item.extents}</div>
+              <div>{item.plZone}</div>
+            </>
+          ))}
+        </div>
+      )}
+    </div>
   );
 }
--- src/data/mockdata.js
+++ src/data/mockdata.js
@@ -1,15 +1,50 @@
-export const location = [
-  { label: 'Reg. num', value: '#8/123' },
-  { label: 'District', value: 'Lefkosia' },
-  { label: 'Municipality', value: 'Strovolos' },
-  { label: 'Quarter', value: 'Agios Vasileios' },
-  /* {label: "Land", parcels: [
-        { regNumber: "#8/759", area: 586, planningZone: "Kα5, Kα6", value: 32000 },
-        { regNumber: "#8/909", area: 419, planningZone: "Kα5", value: 50000 },
-    ]},
-    {label: "Units", buildings: [
-        { regNumber: "#8/3220", BuildingType: "flat", extents: "143 / 18 / 26", planningZone: "Kα5, Kα6", value: 165100, },
-        { regNumber: "#8/3221", BuildingType: "flat", extents: "143 / 18 / - ", planningZone: "Kα5", value: 158100, },
-        { regNumber: "#8/3222", BuildingType: "flat", extents: "143 / 18 / - ", planningZone: "Kα5", value: 158100, },
-    ]},*/
-];
+export const location = {
+  generalInfo: {
+    regNumber: '#8/123',
+    district: 'Lefkosia',
+    municipality: 'Strovolos',
+    quarter: 'Agios Vasileios',
+  },
+  land: [
+    {
+      regNumber: '#8/759',
+      area: 586,
+      plZone: 'Kα5, Kα6',
+      value: 32000,
+      property: 'parcel',
+    },
+    {
+      regNumber: '#8/909',
+      area: 419,
+      plZone: 'Kα5',
+      value: 50000,
+      property: 'parcel',
+    },
+  ],
+  units: [
+    {
+      regNumber: '#8/3220',
+      type: 'flat',
+      extents: '143 / 18 / 26',
+      plZone: 'Kα5, Kα6',
+      value: 165100,
+      property: 'building',
+    },
+    {
+      regNumber: '#8/3221',
+      type: 'flat',
+      extents: '143 / 18 / -',
+      plZone: 'Kα5',
+      value: 158100,
+      property: 'building',
+    },
+    {
+      regNumber: '#8/3222',
+      type: 'flat',
+      extents: '143 / 18 / -',
+      plZone: 'Kα5',
+      value: 158100,
+      property: 'building',
+    },
+  ],
+};
